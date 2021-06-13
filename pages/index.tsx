9aaf81368e55487ebad6876c7d0edd01625de84e
harrgr <harry@grumbar.me>
2021-06-13 12:12:48 +0100

    Fix footer items for mobile safari

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Home = () => {
   return (
     <Layout>
-      <div className="bg-gray-600 fixed  z-[-1] h-screen w-screen">
+      <div className="bg-gray-600 fixed z-[-1] h-screen w-screen">
         <Image
           alt="Mountains"
           src="/images/daniel-leone-g30P1zcOzXo-unsplash.jpg"
@@ -15,47 +15,46 @@ const Home = () => {
           className="text-transparent"
         />
       </div>
-      <div className="min-h-screen p-8 pb-16 md:px-32 md:pt-16 text-white flex flex-col">
-        <header className="space-y-4 w-full">
-          <h1 className="text-4xl font-bold tracking-tighter drop-shadow">
-            Harry Grumbar
-          </h1>
-          <p>
-            Software Engineer based in London, UK. Currently working at{" "}
-            <a
-              href="https://bulb.co.uk"
-              target="_blank"
-              className="hover:text-gray-200 underline"
-            >
-              Bulb
-            </a>
-            .
-          </p>
-        </header>
-        <main className="w-full"></main>
-        <footer className="w-full mt-auto text-sm flex justify-between">
-          <nav>
-            <ul className="space-x-4">
-              <li className="inline-block">
-                <a href="https://twitter.com/harrygrumbar" target="_blank">
-                  Twitter
-                </a>
-              </li>
-              <li className="inline-block">
-                <a href="https://github.com/harrygr" target="_blank">
-                  Github
-                </a>
-              </li>
-              <li className="inline-block">
-                <a href="mailto:harry@grumbar.me" target="_blank">
-                  Email
-                </a>
-              </li>
-            </ul>
-          </nav>
-          <p>&copy; 2020</p>
-        </footer>
-      </div>
+
+      <header className="space-y-4 w-full p-8 md:px-32 md:pt-16 text-white">
+        <h1 className="text-4xl font-bold tracking-tighter drop-shadow">
+          Harry Grumbar
+        </h1>
+        <p>
+          Software Engineer based in London, UK. Currently working at{" "}
+          <a
+            href="https://bulb.co.uk"
+            target="_blank"
+            className="hover:text-gray-200 underline"
+          >
+            Bulb
+          </a>
+          .
+        </p>
+      </header>
+
+      <footer className="w-full mt-auto text-sm flex justify-between fixed bottom-0 inset-x-0 p-8 md:px-32 pb-16 text-white">
+        <nav>
+          <ul className="space-x-4">
+            <li className="inline-block">
+              <a href="https://twitter.com/harrygrumbar" target="_blank">
+                Twitter
+              </a>
+            </li>
+            <li className="inline-block">
+              <a href="https://github.com/harrygr" target="_blank">
+                Github
+              </a>
+            </li>
+            <li className="inline-block">
+              <a href="mailto:harry@grumbar.me" target="_blank">
+                Email
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <p>&copy; 2020</p>
+      </footer>
     </Layout>
   );
 };
