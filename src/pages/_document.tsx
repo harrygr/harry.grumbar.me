c29c4f74da7ba93bfeb89564ad4708e31f0f5e0e
Harry Grumbar <harry@grumbar.me>
2022-05-04 17:15:20 +0100

    Add umami script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,12 @@ class Document extends NextDocument {
         <Head>
           <link rel="icon" href="/favicon.svg" />
           <meta name="theme-color" content="#fff" />
+          <script
+            async
+            defer
+            data-website-id="049a950f-3150-4194-a086-64da434ce6d1"
+            src="https://umami-hg.fly.dev/umami.js"
+          ></script>
         </Head>
         <body className="h-full antialiased font-body">
           <Main />
