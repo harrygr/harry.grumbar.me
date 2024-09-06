8e8b252aaf73f25979e178c6113553fae49935fe
Harry <harry@grumbar.me>
2024-09-06 17:23:42 +0200

    Add Github actions

--- app/components/cv/AboutMe.tsx
+++ app/components/cv/AboutMe.tsx
@@ -11,20 +11,21 @@ export const AboutMe: React.FC = () => {
     <section>
       <h2 id="about-me">About Me</h2>
       <p>
-        I'm a full-stack product engineer with strengths working in agile teams
-        using industry-standard tools and libraries with over {yearsExperience}{" "}
-        years' experience building and delivering software professionally.
+        I&apos;m a full-stack product engineer with strengths working in agile
+        teams using industry-standard tools and libraries with over{" "}
+        {yearsExperience} years&apos; experience building and delivering
+        software professionally.
       </p>
       <p>
         I have experience modeling complex domains for backend systems and a
         focus on testable and predictable code is core to my programming
-        principles. I'm a strong proponent of functional programming in modern
-        scalable applications and believe it results in fewer bugs, easier
-        testing and more comprehensible code.
+        principles. I&apos;m a strong proponent of functional programming in
+        modern scalable applications and believe it results in fewer bugs,
+        easier testing and more comprehensible code.
       </p>
       <p>
-        I've led small product engineering teams and have extensive experience
-        with prioritising workloads, managing backlogs, and working
+        I&apos;ve led small product engineering teams and have extensive
+        experience with prioritising workloads, managing backlogs, and working
         collaboratively with product to effectively deliver working software.
       </p>
     </section>
